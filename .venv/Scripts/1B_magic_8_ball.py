50ee7f088c9d2015bc7c5d63a7626d410f8854ff
DuckSilver <silverdungeonsy@gmail.com>
2024-08-28 19:00:21 -0400

    Add files via upload

---
+++ .venv/Scripts/1B_magic_8_ball.py
@@ -0,0 +1,52 @@
+# Alex Lewis / Assignment 1B
+# The system simulates a magic 8 ball. Giving the user a random yes/no answer
+import random
+def main():
+
+    def eightball():
+        # creating/opening the txt file
+        file1 = open("8ball_responses.txt", "w")
+
+        # writes the possible outputs of the program to the txt file
+        file1.write("Yes, of course! \n")
+        file1.write("Without a doubt, yes. \n")
+        file1.write("You can count on it. \n")
+        file1.write("For sure! \n")
+        file1.write("Ask me later! \n")
+        file1.write("I'm not sure. \n")
+        file1.write("I can't tell you right now. \n")
+        file1.write("I'll tell you after my nap. \n")
+        file1.write("No way! \n")
+        file1.write("I don't think so. \n")
+        file1.write("Without a doubt, no. \n")
+        file1.write("The answer is clearly NO! \n")
+
+        file1.close()
+
+
+        # reads the txt file onto a list: ball
+        file1 = open("8ball_responses.txt", "r+")
+
+        data = file1.read()
+        ball = data.split("\n")
+
+        # function which selects then prints a random response from the list
+        def answer():
+            num = random.randint(0,11)
+            print("........")
+            print(ball[num])
+            print()
+
+        # prompts the user to use the 8 ball
+        cont = input("Use magic 8 ball? (y/n): ")
+        while cont != "n":
+            answer()
+            cont = input("Continue? (y/n): ")
+
+
+
+
+    eightball()
+
+main()
+
